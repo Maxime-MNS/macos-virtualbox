93e8d84db1aa7e967c36efe9176f0f598d827075
Maxime Jacquet <maxime@morenewssoon.com>
2021-03-27 23:35:29 +0100

    Add personnal config

--- macos-guest-virtualbox.sh
+++ macos-guest-virtualbox.sh
@@ -19,8 +19,8 @@ vm_name="macOS"                  # name of the VirtualBox virtual machine
 macOS_release_name="Catalina"    # install "HighSierra" "Mojave" or "Catalina"
 storage_size=80000               # VM disk image size in MB, minimum 22000
 storage_format="vdi"             # VM disk image file format, "vdi" or "vmdk"
-cpu_count=2                      # VM CPU cores, minimum 2
-memory_size=4096                 # VM RAM in MB, minimum 2048
+cpu_count=4                      # VM CPU cores, minimum 2
+memory_size=8192                 # VM RAM in MB, minimum 2048
 gpu_vram=128                     # VM video RAM in MB, minimum 34, maximum 128
 resolution="1280x800"            # VM display resolution
 
@@ -33,21 +33,21 @@ resolution="1280x800"            # VM display resolution
 
 #   system_profiler SPHardwareDataType
 DmiSystemFamily="MacBook Pro"        # Model Name
-DmiSystemProduct="MacBookPro11,2"    # Model Identifier
-DmiSystemSerial="NO_DEVICE_SN"       # Serial Number (system)
-DmiSystemUuid="CAFECAFE-CAFE-CAFE-CAFE-DECAFFDECAFF" # Hardware UUID
+DmiSystemProduct="MacBookPro13,2"    # Model Identifier
+DmiSystemSerial="C02SPB4LGTFJ"       # Serial Number (system)
+DmiSystemUuid="1338B3BF-9AB5-5DCA-BE9C-2418F26C9090" # Hardware UUID
 DmiBIOSVersion="string:MBP7.89"      # Boot ROM Version
 DmiOEMVBoxVer="string:1"             # Apple ROM Info - left of the first dot
 DmiOEMVBoxRev="string:.23.45.6"      # Apple ROM Info - first dot and onward
 #   ioreg -l | grep -m 1 board-id
-DmiBoardProduct="Mac-3CBD00234E554E41"
+DmiBoardProduct="Mac-66E35819EE2D0D05"
 #   nvram 4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14:MLB
 DmiBoardSerial="NO_LOGIC_BOARD_SN"    # stored in EFI
 MLB="${DmiBoardSerial}"               # stored in NVRAM
 #   nvram 4D1EDE05-38C7-4A6A-9CC6-4BCCA8B38C14:ROM
 ROM='%aa*%bbg%cc%dd'
 #   ioreg -l -p IODeviceTree | grep \"system-id
-SYSTEM_UUID="aabbccddeeff00112233445566778899"
+SYSTEM_UUID="cd496cd42000285ca565fbe1ab7121d0"
 #   csrutil status
 SYSTEM_INTEGRITY_PROTECTION='10'  # '10' - enabled, '77' - disabled
 
